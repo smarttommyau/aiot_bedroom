ce1e11322471b99e096515ed45c6e564e333b5fb
smarttommyau <smarttommyau@gmail.com>
2023-02-03 21:10:38 +0800

    update

--- ai_analysis/live_analysis.py
+++ ai_analysis/live_analysis.py
@@ -1,6 +1,13 @@
 from live_connection import Live_connection
 import threading
+import torch
+from PIL import Image
+import io
 
+model = torch.hub.load('ultralytics/yolov5', 'yolov5x6', pretrained=True)
+model.classes = [0,59,63,67]
+torch.set_num_interop_threads(8)
+torch.set_num_threads(8)
 
 live_connection = Live_connection(7777)
 
@@ -11,7 +18,10 @@ t1.start()
 
 while True :
     #get data
-    thermal_data = live_connection.getcurrentframe()
+    if live_connection.new_frame_avaliable:
+        (frame,thermaldata)= live_connection.getcurrentframe()
+        image = Image.open(io.BytesIO)
+        
     #predict with model
 
     
