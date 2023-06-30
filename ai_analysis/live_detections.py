33ec3c245ac17ddb9f5f792fb9d756737e441b05
smarttommyau <smarttommyau@gmail.com>
2023-07-01 01:02:29 +0800

    Update for detection finished

--- ai_analysis/live_detections.py
+++ ai_analysis/live_detections.py
@@ -1,73 +1,79 @@
 import numpy as np
 import torch
 import threading
+import live_status_manager
 class Person:
     def __init__(self):
         self.box = None
-        self.lying_bed = False
-        self.touching_phone = False
+        self.lying_bed = StatusManager(2,2)
+        self.touching_phone = StatusManager(2,2)
+        self.moving = StatusManager(2,10)
+        self.sleeping = StatusManager()
         self.temperature = 0
         self.xyxy = torch.tensor([0,0,0,0])
-        self.moving = False
-        self.notmoving = 0
     ## This function must be called before other operations
     def update_box(self,box):
         self.box = box
-
-    def update_lying_bed(self,bed):
+    
+    def update_lying_bed(self,bed,timenow,event):
         box = self.box
-        self.lying_bed = False
+        lying_bed = False
         if(bed is None):
             ## fallback to only checking posture is lying or not
             length = max(box.xywh[2],box.xywh[3])
             width = min(box.xywh[2],box.xywh[3])
             if(length/width>3 and length/width<5):
-                self.lying_bed = True
+                lying_bed = True
         else:
             ## check if the person is on the bed
             if torch.all(box.xyxy[0:2] >= bed.box.xyxy[0:2]) and torch.all(box.xyxy[2:4] <= bed.box.xyxy[2:4]):
                 ## check if person is lying
                 if bed.box.xywh[2] > bed.box.xywh[3]:
-                    if box.xywh[2]/box.xywh[3] > 0.6:
-                        self.lying_bed = True
+                        lying_bed = True
                 else:
                     if box.xywh[3]/box.xywh[2] > 0.6:
-                        self.lying_bed = True
-    def update_touching_phone(self,phones):
+                        lying_bed = True
+        event.set()
+        self.lying_bed.update_status(lying_bed,timenow)
+    def update_touching_phone(self,phones,timenow,event):
         box = self.box
-        self.touching_phone = False
         for phone in phones:
             if torch.all(phone.box.xyxy[0:2] >= box.xyxy[0:2]) and torch.all(phone.box.xyxy[2:4] <= box.xyxy[2:4]):
-                self.touching_phone = True
+                self.touching_phone.update_status(True,timenow)
                 return
-    def update_moving(self):
-        # tolerance of movement is 10 pixels
+        self.touching_phone.update_status(False,timenow)
+        event.set()
+    def update_moving(self,timenow,event):
         xyxy = self.box.xyxy
-        if torch.all(torch.abs(self.xyxy-xyxy)<10):
-            self.moving = False
-            self.notmoving += 1
-        else:
-            self.moving = True
+        # tolerance of movement is 10 pixels
+        self.moving.update_status(torch.all(torch.abs(self.xyxy-xyxy)>=10,timenow))
         self.xyxy = xyxy
+        event.set()
 
-    def update_temperature(self,thermal,other_object):
+    def update_temperature(self,thermal,other_object,event):
         (x1,y1,x2,y2) = self.box.xyxy
         thermal = thermal * other_object
         self.temperature = np.sum(thermal[x1:x2+1][y1:y2+1]/100 - 273)/(x2-x1+1)/(y2-y1+1)
+        event.set()
 
-
-
-
+    def update_sleeping(self,event,lying_bed,touching_phone):
+        lying_bed.wait(),touching_phone.wait()
+        if self.lying_bed.status and not self.touching_phone.status:
+            self.sleeping = True
+        else:
+            self.sleeping = False
+        event.set()
 class Bed:
     def __init__(self,box):
         self.box = box
-    def update_temperature(self,thermal,other_object,personxyxy):
+    def update_temperature(self,thermal,other_object,personxyxy,event):
         (x1,y1,x2,y2) = self.box.xyxy
         thermal = thermal * other_object
         ## set overlap with person to 0
         (x1p,y1p,x2p,y2p) = personxyxy
         thermal[x1p:x2p+1][y1p:y2p+1] = 0
         self.temperature = np.sum(thermal[x1:x2+1][y1:y2+1]/100 - 273)/(x2-x1+1)/(y2-y1+1)
+        event.set()
     
 
 class Phone:
@@ -77,13 +83,16 @@ class Phone:
 class detection:
     def __init__(self):
         self.person = Person()
-        self.lock = False
-    def update(self,result,thermal) -> list[threading.Thread]:
+        self.person_prensence = StatusManager(2,2)
+        self.lock = threading.Lock()
+        self.timenow = 0
+        self.events = tuple(threading.Event() * 6)
+        ## 0: lying_bed, 1: touching_phone, 2: moving, 3: temperature, 4: bed_temperature, 5: sleep
+    def update(self,result,thermal,timenow):
+        self.lock.aquire()
         ## timenow is now perserve, may still use later is fps is fluctuating too much
         # Persons = list()
-        while self.lock:
-            pass
-        self.lock = True
+        self.timenow = timenow
         phones =  list()
         bed = None
         updated = False
@@ -93,7 +102,7 @@ class detection:
                 ## cannot actually does multi user
                 # Persons.append(Person(box))
                 if updated:
-                    pass
+                    continue
                 self.person.update_box(box)
                 updated = True
                 
@@ -104,16 +113,21 @@ class detection:
                 ## set other object to 1 with box.xyxy
                 (x1,y1,x2,y2) = box.xyxy
                 other_object[x1:x2+1][y1:y2+1] = 0
+        if not updated:
+            if self.person_prensence.update_status(False,timenow):
+                for event in events:
+                    event.set()
+            else:
+                self.lock.release()
+                return
+            
+
         # for person in Persons:
-        threads = tuple(
-            threading.Thread(target=self.person.update_lying_bed,args=(bed,)),
-            threading.Thread(target=self.person.update_touching_phone,args=(phones,)),
-            threading.Thread(target=self.person.update_moving,args=()),
-            threading.Thread(target=self.person.update_temperature,args=(thermal,other_object))
-            threading.Thread(target=bed.update_temperature,args=(thermal,other_object,person.box.xyxy)) if bed is not None else None
-        )
-        for thread in threads:
-            thread.start()
-        return threads
-    def free(self):
-        self.lock = False
+        threading.Thread(target=self.person.update_sleeping,args=(self.events[5],self.events[0],self.events[1])).start()
+        threading.Thread(target=self.person.update_lying_bed,args=(bed,timenow,self.events[0])).start()
+        threading.Thread(target=self.person.update_touching_phone,args=(phones,timenow,self.events[1])).start()
+        threading.Thread(target=self.person.update_moving,args=(timenow,self.events[2])).start()
+        threading.Thread(target=self.person.update_temperature,args=(thermal,other_object,self.evnets[3])).start()
+        if bed is not None:
+            threading.Thread(target=bed.update_temperature,args=(thermal,other_object,self.person.box.xyxy,self.events[4])).start()
+            self.events[4].set()
---
+++ ai_analysis/live_status_manager.py
@@ -0,0 +1,27 @@
+class StatusManager:
+    ## Tolerance to prevent false negative or false positive
+    def __init__(self,tolerances_positive=0,tolerance_negative=0,status=False) -> None:
+        self.__default_tolerance_positive = tolerances_positive## to become positive
+        self.__default_tolerance_negative = tolerance_negative## to become negative
+        self.__counter_tolerance_positive = tolerances_positive
+        self.__counter_tolerance_negative = tolerance_negative
+        self.status  = status
+        self.start = 0
+    def update_status(self,status,timenow) -> bool:
+        if status == self.status:
+            self.__counter_tolerance_positive = self.__default_tolerance_positive
+            self.__counter_tolerance_negative = self.__default_tolerance_negative
+            return False
+        if status:
+            self.__counter_tolerance_negative -= 1
+            if self.__counter_tolerance_negative <=0:
+                self.status = False
+                return True
+        else:
+            self.__counter_tolerance_positive -= 1
+            if self.__counter_tolerance_positive <=0:
+                self.status = True
+                self.start = timenow
+                return True
+
+            
