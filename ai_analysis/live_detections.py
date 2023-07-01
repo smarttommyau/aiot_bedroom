fa873f8a93639c10004f7e057d701a1ef4f7c86e
smarttommyau <smarttommyau@gmail.com>
2023-07-01 18:44:02 +0800

    Bug fixes

--- ai_analysis/live_detections.py
+++ ai_analysis/live_detections.py
@@ -20,6 +20,8 @@ class Person:
     def update_lying_bed(self,bed,timenow,event):
         self.logger.info("Lying bed updating...")
         box = self.box
+        xyxy = box.xyxy.flatten()
+        bxyxy = bed.box.xyxy.flatten()
         lying_bed = False
         if(bed is None):
             ## fallback to only checking posture is lying or not
@@ -29,7 +31,7 @@ class Person:
                 lying_bed = True
         else:
             ## check if the person is on the bed
-            if torch.all(box.xyxy[0:2] >= bed.box.xyxy[0:2]) and torch.all(box.xyxy[2:4] <= bed.box.xyxy[2:4]):
+            if torch.all(xyxy[0:2] >= bxyxy[0:2]) and torch.all(xyxy[2:4] <= bxyxy[2:4]):
                 ## check if person is lying
                 if bed.box.xywh[2] > bed.box.xywh[3]:
                         lying_bed = True
@@ -40,9 +42,11 @@ class Person:
         self.lying_bed.update_status(lying_bed,timenow)
     def update_touching_phone(self,phones,timenow,event):
         self.logger.info("Touching phone updating...")
-        box = self.box
+        xyxy = self.box.xyxy.flatten()
+        pxyxy = phone.box.xyxy.flatten()
+
         for phone in phones:
-            if torch.all(phone.box.xyxy[0:2] >= box.xyxy[0:2]) and torch.all(phone.box.xyxy[2:4] <= box.xyxy[2:4]):
+            if torch.all(pxyxy[0:2] >= xyxy[0:2]) and torch.all(pxyxy[2:4] <= xyxy[2:4]):
                 self.touching_phone.update_status(True,timenow)
                 return
         self.touching_phone.update_status(False,timenow)
@@ -51,24 +55,27 @@ class Person:
         self.logger.info("Moving updating...")
         xyxy = self.box.xyxy
         # tolerance of movement is 10 pixels
-        self.moving.update_status(torch.all(torch.abs(self.xyxy-xyxy)>=10,timenow))
+        if torch.all(torch.abs(xyxy - self.xyxy) < 10):
+            self.moving.update_status(False,timenow)
+        else:
+            self.moving.update_status(True,timenow)
         self.xyxy = xyxy
         event.set()
 
     def update_temperature(self,thermal,other_object,event):
         self.logger.info("Temperature updating...")
-        (x1,y1,x2,y2) = self.box.xyxy
+        (x1,y1,x2,y2) = self.box.xyxy.flatten()
         thermal = thermal * other_object
         self.temperature = np.sum(thermal[x1:x2+1][y1:y2+1]/100 - 273)/(x2-x1+1)/(y2-y1+1)
         event.set()
 
-    def update_sleeping(self,event,lying_bed,touching_phone):
+    def update_sleeping(self,timenow,event,lying_bed,touching_phone):
         lying_bed.wait(),touching_phone.wait()
         self.logger.info("Sleeping updating...")
+        sleeping = False
         if self.lying_bed.status and not self.touching_phone.status:
-            self.sleeping = True
-        else:
-            self.sleeping = False
+            sleeping = True
+        self.sleeping.update_status(sleeping,timenow)
         event.set()
 class Bed:
     def __init__(self,logger):
@@ -80,7 +87,7 @@ class Bed:
         self.box = box
     def update_temperature(self,thermal,other_object,personxyxy,event):
         self.logger.info("Bed temperature updating...")
-        (x1,y1,x2,y2) = self.box.xyxy
+        (x1,y1,x2,y2) = self.box.xyxy.flatten()
         thermal = thermal * other_object
         ## set overlap with person to 0
         (x1p,y1p,x2p,y2p) = personxyxy
@@ -127,7 +134,7 @@ class detection:
             elif box.cls == 63 or box.cls == 67:
                 phones.append(Phone(box))
                 ## set other object to 1 with box.xyxy
-                (x1,y1,x2,y2) = box.xyxy
+                (x1,y1,x2,y2) = box.xyxy.flatten()
                 other_object[x1:x2+1][y1:y2+1] = 0
         self.logger.info("Lock aquired")
         self.lock.acquire(blocking=True)
@@ -150,13 +157,13 @@ class detection:
             self.person_prensence.update_status(True,timenow)
 
         # for person in Persons:
-        threading.Thread(target=self.person.update_sleeping,args=(self.events[5],self.events[0],self.events[1])).start()
+        threading.Thread(target=self.person.update_sleeping,args=(timenow,self.events[5],self.events[0],self.events[1])).start()
         threading.Thread(target=self.person.update_lying_bed,args=(self.bed,timenow,self.events[0])).start()
         threading.Thread(target=self.person.update_touching_phone,args=(phones,timenow,self.events[1])).start()
         threading.Thread(target=self.person.update_moving,args=(timenow,self.events[2])).start()
-        threading.Thread(target=self.person.update_temperature,args=(thermal,other_object,self.evnets[3])).start()
+        threading.Thread(target=self.person.update_temperature,args=(thermal,other_object,self.events[3])).start()
         if bboxs is not None:
-            threading.Thread(target=self.bed.update_temperature,args=(thermal,other_object,self.person.box.xyxy,self.events[4])).start()
+            threading.Thread(target=self.bed.update_temperature,args=(thermal,other_object,self.person.box.xyxy.flatten(),self.events[4])).start()
         else:
             self.events[4].set()
         self.lock.release()
