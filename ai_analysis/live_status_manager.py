041fd6aae2705183be30c1795e9a137d5f8a749d
smarttommyau <smarttommyau@gmail.com>
2023-07-05 01:26:42 +0800

    average manager(Future use for: kE manager)

--- ai_analysis/live_status_manager.py
+++ ai_analysis/live_status_manager.py
@@ -1,4 +1,6 @@
 from time import time
+from collections import deque
+from statistics import mean
 class StatusManager:
     ## Tolerance to prevent false negative or false positive
     def __init__(self,tolerances_positive=0,tolerance_negative=0,status=False) -> None:
@@ -32,4 +34,28 @@ class StatusManager:
                 return True
         return False
 
-            
+class AverageManagerByValue:
+    # deque seems to have a significant edge against list 
+    def __init__(self,numOf_value=10) -> None:
+        self.__deque = deque(maxlen=numOf_value)
+        self.average = 0
+        self.__numOf_value = numOf_value
+    def update_value(self,value):
+        self.__deque.append(value)
+        self.average = sum(self.__deque)/self.__numOf_value
+
+
+class AverageManagerByTime:
+    ## Use list as it has better stability while they have similar performance even in scale
+    def __init__(self,period=10) -> None:
+        self.__list = []
+        self.__list_time = []
+        self.average = 0
+        self.__period = period
+    def update_value(self,value,timenow):
+        self.__list.append(value)
+        self.__list_time.append(timenow)
+        while self.__list_time[0] < timenow - self.__period:
+            self.__list_time.pop(0)
+            self.__list.pop(0)
+        self.average = mean(self.__list)
